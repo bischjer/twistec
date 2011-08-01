0c82d1a394a61f432e0f0818683cb4284556ff06
bischjer <bischjer@io.(none)>
2011-08-01 23:54:30 +0200

    update

--- log.h
+++ log.h
@@ -1,7 +1,14 @@
+#ifndef LOG_H
+#define LOG_H
+
 #include <stdio.h>
-#define log(fmt, ...) printf(("INFO [%s:%d]: " fmt "\n"), __FILE__, __LINE__, ##__VA_ARGS__);
+
+#define log(fmt, ...) printf( ("INFO [%s:%d]: " fmt "\n"), __FILE__, __LINE__, ##__VA_ARGS__);
 #ifdef DEBUG
 #define log_debug(fmt, ...) printf(("DEBUG [%s:%d]: " fmt "\n"), __FILE__, __LINE__, ##__VA_ARGS__);
 #else
 #define log_debug(...);
+
+#endif
+
 #endif
