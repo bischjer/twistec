cf1a40fa501d6395832bf6d4ef66c77d58edc951
David Buchmann <david.buchmann@tandberg.com>
2011-09-09 16:02:41 +0200

    Fixing ifndef for log.cpp

--- log.cpp
+++ log.cpp
@@ -1,7 +1,9 @@
+#ifndef log
 #       define log(fmt, ...) printf(("[%s:%d] %s: " fmt "\n"), __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__);
+#endif
 
 #ifdef DEBUG
 #       define log_debug(fmt, ...) printf(("[%s:%d] %s: " fmt "\n"), __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__);
 #else
-#       define log(...)
+#       define log_debug(...)
 #endif
