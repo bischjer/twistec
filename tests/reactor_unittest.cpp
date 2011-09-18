8e12af08fb6681f056014def3748a0c36594b9f3
David Buchmann <david.buchmann@gmail.com>
2011-09-19 01:07:47 +0200

    Add check that callback is run in unittest.

--- tests/reactor_unittest.cpp
+++ tests/reactor_unittest.cpp
@@ -2,8 +2,11 @@
 #include "reactor.hpp"
 #include "gtest/gtest.h"
 
+bool ran_callback = false;
+
 void is_running(void)
 {
+	ran_callback = true;
 	Reactor* reactor = Reactor::getInstance();
 	EXPECT_TRUE(reactor->is_running());
 	reactor->stop();
@@ -11,7 +14,10 @@ void is_running(void)
 
 TEST(ReactorTest, run_and_stop) {
 	Reactor* reactor = Reactor::getInstance();
+	EXPECT_FALSE(reactor->is_running());
 	reactor->callLater(0, &is_running);
+	EXPECT_FALSE(reactor->is_running());
 	reactor->run();
 	EXPECT_FALSE(reactor->is_running());
+	EXPECT_TRUE(ran_callback);
 }
